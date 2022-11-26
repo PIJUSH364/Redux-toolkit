6579f5ee8d5dc4721a4ada1e5906c0725ee16087
PIJUSH364 <praymondal@gmail.com>
2022-11-26 20:22:51 +0530

    stop content

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 ## react redux
+
+## step1:
+
+create a store file in data folder
+
+## step2:
+
+configure store funcationality
+
+## step3:
+
+index.js in that file app component by provider from reacr-redux
+
+## step4:
+
+in provider component store props pass
+
+## step5:
+
+createing redux action and reducer
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+function Navbar() {
+  return <div>navbar</div>;
+}
+
+export default Navbar;
+{
+  /* <NavLink to="/">home</NavLink>
+      <NavLink to="/cart">
+        Cart <sub>7</sub>
+      </NavLink>
+      <NavLink to="/">bbnh</NavLink> */
+}
